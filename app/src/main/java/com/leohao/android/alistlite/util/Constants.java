2b69ede9fd9f7da14e1e69c9a5a38fcdf5611068
Wenhao Zhao <2867555086@qq.com>
2024-02-27 22:15:20 +0800

    update: 删除不必要常量

--- app/src/main/java/com/leohao/android/alistlite/util/Constants.java
+++ app/src/main/java/com/leohao/android/alistlite/util/Constants.java
@@ -9,6 +9,4 @@ public class Constants {
     public static String URL_ABOUT_BLANK = "about:blank";
     public static String QUICK_DOWNLOAD_ADDRESS = "https://pan.leohao.cn/d/LeoHao/files/share/APP/AListLite";
     public static String BUGLY_APP_ID = "8a7088fc39";
-    public static String BUGLY_APP_KEY = "df85fde5-e5a3-4992-80e5-815e43042a1e";
-    public static String PERMISSION_APPLY_MSG = "为保证软件正常运行，请授予相关权限";
 }
