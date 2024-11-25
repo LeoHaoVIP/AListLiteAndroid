63879805f0d9ca791b2f07719a42ff1a9f60e7ab
Wenhao Zhao <2867555086@qq.com>
2024-11-25 23:01:05 +0800

    update: 更新 bugly 日志上报插件配置

--- app/src/main/java/com/leohao/android/alistlite/AlistLiteApplication.java
+++ app/src/main/java/com/leohao/android/alistlite/AlistLiteApplication.java
@@ -3,7 +3,6 @@ package com.leohao.android.alistlite;
 import android.app.Application;
 import android.content.Context;
 import android.os.Build;
-import com.leohao.android.alistlite.util.Constants;
 import com.tencent.bugly.crashreport.CrashReport;
 
 /**
@@ -19,6 +18,6 @@ public class AlistLiteApplication extends Application {
         CrashReport.UserStrategy strategy = new CrashReport.UserStrategy(getApplicationContext());
         //获取设备型号
         strategy.setDeviceModel(Build.MODEL);
-        CrashReport.initCrashReport(getApplicationContext(), Constants.BUGLY_APP_ID, false, strategy);
+        CrashReport.initCrashReport(getApplicationContext(), strategy);
     }
 }
